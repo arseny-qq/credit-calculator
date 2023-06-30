c05fff784316a5195c7a81a5bece12581fb4cedf
Аросланкин Арсений Юрьевич <aaroslankin@phoenixit.ru>
2023-06-30 22:30:04 +0300

    module 5: rdy

--- js/view/costInput.js
+++ js/view/costInput.js
@@ -1,5 +1,6 @@
 const init = (getData) => {
     const $input = document.querySelector(`#input-cost`)
+    const { cost, minPrice, maxPrice } = getData()
 
     const settings = {
         numeral: true,
@@ -7,7 +8,34 @@ const init = (getData) => {
         delimiter: ` `
     }
 
-    new Cleave($input, settings)
+    const cleaveInput = new Cleave($input, settings)
+    cleaveInput.setRawValue(cost)
+
+    $input.addEventListener(`input`, () => {
+        const $wrapper = $input.closest(`.param__details`)
+        const value = +cleaveInput.getRawValue()
+
+        if (value < minPrice || value > maxPrice) {
+            $wrapper.classList.add(`param__details--error`)
+        }
+        else {
+            $wrapper.classList.remove(`param__details--error`)
+        }
+    })
+
+    $input.addEventListener(`change`, () => {
+        const $wrapper = $input.closest(`.param__details`)
+        const value = +cleaveInput.getRawValue()
+
+        if (value < minPrice) {
+            cleaveInput.setRawValue(minPrice)
+        }
+        else if (value > maxPrice) {
+            cleaveInput.setRawValue(maxPrice)
+        }
+
+        $wrapper.classList.remove(`param__details--error`)
+    })
 }
 
 
